e1d435a913b4293be309f17adb132a00b85c6e6a
d.gedroit <d.gedroit@gedroitd-l>
2018-03-22 11:33:01 +0300

    Changed configuration of Docker

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,13 @@
 FROM python:3.5
 
+#TODO: make this file more readable
+
 LABEL creator='FUNNYDMAN'
 
-ENV SOURCE_PATH /source
 
+ENV SOURCE_PATH /source
 RUN mkdir $SOURCE_PATH
+
 
 WORKDIR $SOURCE_PATH
 
@@ -13,12 +16,13 @@ ADD . $SOURCE_PATH/
 RUN pip install -r requirements.txt
 
 
-COPY ./server/entrypoint.sh /source/server/entrypoint.sh
 
-RUN chmod +x /source/server/entrypoint.sh
+
+WORKDIR $SOURCE_PATH
+COPY ./server/entrypoint.sh $SOURCE_PATH/server/entrypoint.sh
+RUN chmod +x $SOURCE_PATH/server/entrypoint.sh
 
 WORKDIR $SOURCE_PATH/server/
-
 RUN ./entrypoint.sh
 
 WORKDIR $SOURCE_PATH
--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 PYTHON_INTERPRETER=$(which python3)
-
+APPLICATION_ADDRESS=0.0.0.0
+APPLICATION_PORT=8000
 
 function run_migrations {
     echo "Running database migrations."
@@ -12,10 +13,15 @@ function collect_static {
     echo "Collecting application static files."
     $PYTHON_INTERPRETER manage.py collectstatic --noinput
 }
+function run_dev_server {
+    echo "Running application server. ($APPLICATION_ADDRESS:$APPLICATION_PORT)"
+    $PYTHON_INTERPRETER manage.py runserver --nothreading $APPLICATION_ADDRESS:$APPLICATION_PORT
+}
 
 function main() {
     run_migrations
     collect_static
+#    run_dev_server
 }
 
 main
