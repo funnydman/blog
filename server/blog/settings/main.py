837b704e5b5e4edcc909740c1d48703f2004bacb
d.gedroit <d.gedroit@gedroitd-l>
2018-04-28 17:19:49 +0300

    add docker-compose for prod and configure settings

--- server/blog/settings/main.py
+++ server/blog/settings/main.py
@@ -1,18 +1,21 @@
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(
+    os.path.dirname(
+        os.path.dirname(
+            os.path.abspath(__file__)
+        )
+    )
+)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9mxq#0nu(t9uysm)l+)ky#ew0s&rl#&xdub#*zs7a)ar!!@%d%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -43,7 +46,7 @@ ROOT_URLCONF = 'blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -63,11 +66,11 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'adminblog',
-        'USER': 'adminblog',
-        'PASSWORD': 'adminblog',
+        'NAME': os.environ.get('BLOG_DATABASE_NAME', 'adminblog'),
+        'USER': os.environ.get('BLOG_DATABASE_USER', 'adminblog'),
+        'PASSWORD': os.environ.get('BLOG_DATABASE_PASSWORD', 'adminblog'),
         'HOST': 'database',
-        'PORT': '5432',
+        'PORT': int(os.environ.get('BLOG_DATABASE_PORT', 5432))
     }
 }
 
@@ -105,21 +108,21 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+# just a prefix for urls
 STATIC_URL = '/static/'
 
-# just a prefix for urls
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-# from this paths we get static files
+#
+# # from this paths we get static files
 STATICFILES_DIRS = [
 
-    os.path.join(BASE_DIR, '../client/dist/')
+    os.path.join(BASE_DIR, '../client/dist')
 
 ]
 
 MEDIA_URL = '/media/'
 
 try:
-    from blog.local import *
+    from .local import *
 except ImportError:
     pass
