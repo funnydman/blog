d1d3a3f54f620c858e291315e70a654300ebfbc5
FUNNYDMAN <crazy-dzmitry@mail.ru>
2018-07-29 22:24:29 +0300

    add some tests

--- server/home/tests.py
+++ server/home/tests.py
@@ -1,7 +1,20 @@
 from django.test import TestCase
+from django.urls import reverse
 
 
 class HomeViewsTestCase(TestCase):
+    fixtures = ['file.json']
+
     def test_home(self):
         resp = self.client.get('/')
         self.assertEqual(resp.status_code, 200)
+
+    def test_view_url_accessible_by_name(self):
+        resp = self.client.get(reverse('home'))
+        self.assertEqual(resp.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        resp = self.client.get(reverse('home'))
+        self.assertEqual(resp.status_code, 200)
+
+        self.assertTemplateUsed(resp, 'index.html')
--- server/posts/tests.py
+++ server/posts/tests.py
@@ -5,6 +5,10 @@ from .models import Post
 
 class PostModelTest(TestCase):
 
+    @classmethod
+    def setUpTestData(cls):
+        Post.objects.create(title='test title', content='test content')
+
     def test_create_model_instance(self):
-        post = Post.objects.create(title='test', content='test')
-        self.assertEqual(post.title, 'test')
+        post = Post.objects.get(title='test title')
+        self.assertEqual(post.content, 'test content')
