312b1140591bbdca8bf97656a67ebdca3a739d78
FUNNYDMAN <crazy-dzmitry@mail.ru>
2018-08-08 23:39:14 +0300

    show info if no comment

--- client/app/components/comments/CommentsList.js
+++ client/app/components/comments/CommentsList.js
@@ -2,9 +2,12 @@ import React from "react";
 import Comment from "./Comment";
 
 export default function CommentsList(props) {
+
     if (props.comments === undefined) {
         return <div>Loading comments...</div>;
-    } else {
+    } else if (props.comments.length === 0) {
+        return <div>No comments yet!</div>;
+    }else {
         return (
             <div className="comments-list">
                 {props.comments.map((comment) => (
