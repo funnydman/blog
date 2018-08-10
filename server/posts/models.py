782b23aa2154ed0ed2469841292e51525972b5f3
FUNNYDMAN <crazy-dzmitry@mail.ru>
2018-08-10 11:23:02 +0300

    add category model

--- server/posts/models.py
+++ server/posts/models.py
@@ -2,9 +2,20 @@ from ckeditor_uploader.fields import RichTextUploadingField
 from django.db import models
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=32)
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.name
+
+
 class Post(models.Model):
     title = models.CharField(max_length=250, blank=False, unique_for_date="pub_date")
     content = RichTextUploadingField()
+    category = models.ManyToManyField(Category)
     pub_date = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
 
