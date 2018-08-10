24b6b5b1f0c6a0e651b9aeaf458fd369111dfc16
FUNNYDMAN <crazy-dzmitry@mail.ru>
2018-08-10 19:22:44 +0300

    add is_moderated field to comment model

--- server/comments/admin.py
+++ server/comments/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
+
 from .models import Comment
 
 
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ('post', 'author')
+    list_display = ('post', 'author', 'is_moderated')
 
 
 admin.site.register(Comment, CommentAdmin)
--- server/comments/models.py
+++ server/comments/models.py
@@ -8,6 +8,7 @@ class Comment(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField(max_length=500)
     post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    is_moderated = models.BooleanField(default=False)
 
     def __str__(self):
         return self.post.title
