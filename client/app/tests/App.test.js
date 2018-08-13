b8261302969ee36d970c7a1794ce6a93a38f8ba1
FUNNYDMAN <crazy-dzmitry@mail.ru>
2018-08-13 23:36:54 +0300

    fix jest tests

--- client/app/tests/App.test.js
+++ client/app/tests/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import "isomorphic-fetch"
-import App from '../App';
+import App from '../src/App';
 
 it('renders without crashing', () => {
     const div = document.createElement('div');
