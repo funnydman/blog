179b0e275ac680fcc2026ce7dd91f60e89ef0f95
Dzmitry <dzmitry.hedroits@cogniteq.com>
2019-06-22 10:18:10 +0300

    remove unnecessary packages from requirements

--- client/src/Auth/actions/login.js
+++ client/src/Auth/actions/login.js
@@ -28,7 +28,7 @@ function handleResponse(response) {
             if (response.status === 401) {
                 // auto logout if 401 response returned from api
                 logout();
-                location.reload(true);
+                // location.reload(true);
             }
 
             const error = (data && data.message) || response.statusText;
